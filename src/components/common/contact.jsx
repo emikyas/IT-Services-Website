e1d017232dfff655c1b6966171f079b0bdd721db
Jurjani Valles <jurjani.valles@gmail.com>
2021-06-04 22:23:01 +0800

    Added the OOP contact file

---
+++ src/components/common/contact.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Joi from 'joi-browser';
+import Form from './../common/form';
+
+class Contact extends Form {
+    state = {
+        data: {
+            completeName: '',
+            email: '',
+            company: '',
+            siteLink: '',
+            comments: ''
+        },
+        errors: {}
+    };
+    schema = {
+        id: Joi.string(),
+        completeName: Joi.string().required().label('Complete Name'),
+        email: Joi.string().email({ tlds: {allow: false} }).required().label('Email'),
+        company: Joi.string().required().label('Company Name'),
+        siteLink: Joi.string().uri().required().label('Website Url'),
+        comments: Joi.string().required().label('Comment')
+}
+    render() { 
+        return (
+            <form className="row g-3" onSubmit={this.handleSubmit}>
+                {this.renderInput('completeName', 'Complete Name')}                                        
+                {this.renderInput('email', 'Email')}
+                {this.renderInput('company', 'Company')}
+                {this.renderInput('siteLink', 'Website')}
+                {this.renderInput('comments', 'Comment')}
+                {this.renderButton('Save')}
+            </form>
+        );
+    }
+}
+ 
+export default Contact;
