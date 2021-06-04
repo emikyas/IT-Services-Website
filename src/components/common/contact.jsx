0af7a536e6d50074b34ff5fe5019ddc991866020
Jurjani Valles <jurjani.valles@gmail.com>
2021-06-04 22:26:37 +0800

    Added the OOP Contact file

--- src/components/common/contact.jsx
+++ src/components/common/contact.jsx
@@ -11,7 +11,7 @@ class Contact extends Form {
             siteLink: '',
             comments: ''
         },
-        errors: {}
+        errors: { }
     };
     schema = {
         id: Joi.string(),
